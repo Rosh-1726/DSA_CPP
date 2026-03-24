285772f4fd2e2c460dbbe4f16dcae8436285916e
Rosh-1726 <roshaniahire1702@gmail.com>
2026-03-24 15:09:03 +0530

    2

---
+++ 2_AreaOfCircle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    float area, n;
+    cout<<"Enter radius: ";
+    cin>>n;
+
+    area = 3.14 *n *n;
+    cout<<"Area of Circle : "<<area;
+
+    return 0;
+}
