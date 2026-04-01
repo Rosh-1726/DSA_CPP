8e24d9170fee8dbf32826ca5f0aa2034822b4de8
Rosh-1726 <roshaniahire1702@gmail.com>
2026-04-01 14:18:09 +0530

    basic code

---
+++ Box_Pattern.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+   int size;
+   cout<<"Enter the size of Box: ";
+   cin>>size;
+
+   for(int i =0;i<size ;i++)
+   {
+    for(int j=0;j<size;j++)
+    {
+        if((i==0)||(i==size-1)||(j==0)||(j==size-1))
+        {
+            cout<<"1";
+        }
+        else{
+            cout<<"0";
+        }
+    }
+    cout<<endl;
+   }
+   return 0;
+}
