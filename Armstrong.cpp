dba5a0b0e616c8ab5d8b37d1e84ce1877b59f1be
Rosh-1726 <roshaniahire1702@gmail.com>
2026-04-01 15:38:43 +0530

    basic code

---
+++ Armstrong.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int n ,sum=0,digit=0;
+    cout<<"Enter the number: ";
+    cin>>n;
+    int num = n;
+    while(n!=0)
+    {
+         digit = n % 10;
+         sum = sum+ (digit * digit * digit);
+         n = n/10;
+    }
+    if(sum == num)
+    {
+        cout<<"Number is Armstrong : "<<num;
+    }
+    else
+    {
+        cout<<"Number is not Armstrong: "<<num;
+    }
+    return 0;
+}
